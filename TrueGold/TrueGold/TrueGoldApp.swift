5e115af5bb9fc6dd849dfa17df9262aa6d71bf61
tilo_delau <tdelau@gmail.com>
2025-09-06 17:06:00 +0200

    initial Xcode project

---
+++ TrueGold/TrueGold/TrueGoldApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrueGoldApp.swift
+//  TrueGold
+//
+//  Created by Tilo Delau on 2025-09-06.
+//
+
+import SwiftUI
+
+@main
+struct TrueGoldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
